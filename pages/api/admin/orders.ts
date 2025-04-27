4a46073cb7d3ffb8c8244025e730674e6fe7ba9b
AryaSingh <134718295+Arya-1207@users.noreply.github.com>
2025-04-27 23:53:04 +0530

    Delete pages/api/admin/orders.ts

--- pages/api/admin/orders.ts
+++
@@ -1,6 +0,0 @@
-import { prisma } from "../../../lib/prisma";
-
-export async function GET(req: Request) {
-  const orders = await prisma.order.findMany();
-  return new Response(JSON.stringify(orders), { status: 200 });
-}
